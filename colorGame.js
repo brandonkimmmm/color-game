7f6636e9b43f102a3373c3d5793b37b9c4e199ce
Brandon Kim <bkim2490@gmail.com>
2018-09-25 12:02:48 +0900

    all squares change color when correct one is clicked, new random square selected as answer every new game

--- colorGame.js
+++ colorGame.js
@@ -8,8 +8,9 @@ let colors = [
 ];
 
 let squares = document.querySelectorAll('.square');
-let pickedColor = colors[3];
+let pickedColor = pickColor();
 let colorDisplay = document.getElementById('colorDisplay');
+let messageDisplay = document.querySelector('#message');
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,9 +24,23 @@ for(let i = 0; i < squares.length; i++){
         let clickedColor = this.style.backgroundColor;
         // Compare color to pickedColor
         if(clickedColor === pickedColor){
-            alert('Correct!');
+            messageDisplay.textContent = 'Correct!';
+            changeColors(clickedColor);
         } else {
-            alert('Wrong');
+            this.style.backgroundColor = '#232323';
+            messageDisplay.textContent = 'Try Again';
         }
     });
+};
+
+function changeColors(color){
+    // Change each color to match give color
+    squares.forEach((square) => {
+        square.style.backgroundColor = color;
+    });
+};
+
+function pickColor(){
+    let random = Math.floor(Math.random() * colors.length);
+    return colors[random];
 };
